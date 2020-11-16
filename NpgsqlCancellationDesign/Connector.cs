d3560cf80b1b14cc6bfeb01fdea6e86231a2d4aa
Nikita Kazmin <nkazmin@syntellect.ru>
2020-11-16 14:38:53 +0300

    Made internal visible for tests

--- NpgsqlCancellationDesign/Connector.cs
+++ NpgsqlCancellationDesign/Connector.cs
@@ -5,44 +5,44 @@ namespace NpgsqlCancellationDesign
 {
     public class Connector
     {
-        private readonly ReadBuffer readBuffer;
-        private readonly WriteBuffer writeBuffer;
+        internal ReadBuffer ReadBuffer { get; }
+        internal WriteBuffer WriteBuffer { get; }
 
         internal ReadWriteObject RWO { get; } = new ReadWriteObject();
 
         public int ReadTimeout
         {
-            get => this.readBuffer.Timeout;
-            set => this.readBuffer.Timeout = value;
+            get => this.ReadBuffer.Timeout;
+            set => this.ReadBuffer.Timeout = value;
         }
         public int WriteTimeout
         {
-            get => this.writeBuffer.Timeout;
-            set => this.writeBuffer.Timeout = value;
+            get => this.WriteBuffer.Timeout;
+            set => this.WriteBuffer.Timeout = value;
         }
 
-        public int ReadCtsAllocated => this.readBuffer.CtsAllocated;
-        public int WriteCtsAllocated => this.writeBuffer.CtsAllocated;
+        public int ReadCtsAllocated => this.ReadBuffer.CtsAllocated;
+        public int WriteCtsAllocated => this.WriteBuffer.CtsAllocated;
 
         public Connector()
         {
-            this.readBuffer = new ReadBuffer(this);
-            this.writeBuffer = new WriteBuffer(this);
+            this.ReadBuffer = new ReadBuffer(this);
+            this.WriteBuffer = new WriteBuffer(this);
         }
 
-        public void Write(int value) => this.writeBuffer.Write(async: false, value).GetAwaiter().GetResult();
-        public Task WriteAsync(int value) => this.writeBuffer.Write(async: true, value);
+        public void Write(int value) => this.WriteBuffer.Write(async: false, value).GetAwaiter().GetResult();
+        public Task WriteAsync(int value) => this.WriteBuffer.Write(async: true, value);
 
-        public int Read() => this.readBuffer.Read(async: false).GetAwaiter().GetResult();
+        public int Read() => this.ReadBuffer.Read(async: false).GetAwaiter().GetResult();
         public async Task<int> ReadAsync(CancellationToken cancellationToken = default)
         {
-            using var _ = cancellationToken.Register(this.readBuffer.Cancel);
-            return await this.readBuffer.Read(async: true);
+            using var _ = cancellationToken.Register(this.ReadBuffer.Cancel);
+            return await this.ReadBuffer.Read(async: true);
         }
 
         public void Reset()
         {
-            this.readBuffer.Reset();
+            this.ReadBuffer.Reset();
         }
     }
 }
